ca816c3b19267007e5111cc3a7680e6c2282b17e
yunyin <root@a25j01437.em21>
2017-11-27 10:05:36 +0000

    Arrange Folder

---
+++ src/generator.py
@@ -0,0 +1,130 @@
+# -*- coding:utf-8 -*-
+import os
+import sys
+import time
+
+import numpy as np
+import tensorflow as tf
+import json
+import copy
+
+import data_reader
+import model
+import optimizer
+
+flags = tf.flags
+flags.DEFINE_string("config", None,
+                    "Directory of Config File.")
+FLAGS = flags.FLAGS
+tf.logging.set_verbosity(tf.logging.INFO)
+
+def HParam():
+  if not FLAGS.config:
+    raise ValueError("Must set --config")
+
+  if not os.path.exists(FLAGS.config):
+    raise ValueError("Config File %s Does Not Exist" % (FLAGS.config))
+
+  config = json.load(open(FLAGS.config))
+  if config['data_type'] == 'float16':
+    config['data_type'] = tf.float16
+  else: config['data_type'] = tf.float32
+  return config
+
+def run_epoch(sess, model, data, is_training = False, gen_model = None, vocab = None):
+  data.reset()
+  costs = 0
+  iters = 0
+  times = 0
+  while data.has_next_batch():
+    x_batch, y_batch, mask = data.next_batch()
+    feed_dict = {model.input_data: x_batch,
+                 model.target_data: y_batch,
+                 model.mask: mask}
+
+    fetches = {"costs": model._cost}
+    if is_training: fetches["train_op"] = model.train_op
+
+    vals = sess.run(fetches, feed_dict)
+    costs += vals["costs"]
+    
+    iters += data.seq_length
+    times += 1
+    if times % 2000 == 100:
+      tf.logging('step {}: training_loss:{:4f}'.format(times, np.exp(costs / iters)))
+    if times % 20000 == 0 and vocab != None:
+      sample(sess, model = model, vocab = vocab)
+
+  if gen_model != None and vocab != None:
+    sample(sess, model = gen_model, vocab = vocab)
+  return costs, iters
+
+def sample(sess, model, vocab, max_gen_len = 50):
+  x_batch = np.zeros((1, 1))
+  word = "<s>"
+  state = sess.run(model.cell.zero_state(1, tf.float32))
+
+  output = []
+  while word != '</s>' and len(output) < max_gen_len:
+    x_batch[0][0] = vocab.char2id(word)
+    feed_dict = {model.input_data: x_batch, \
+                 model._initial_state: state}
+    fetches = {"probs": model.probs,
+               "state": model.final_state}
+    vals = sess.run(fetches, feed_dict)
+    p = vals['probs'][0][0]
+    state = vals['state']
+    word = vocab.id2char(np.argmax(p))
+    if word != "</s>": output.append(word)
+
+  line = " ".join(output)
+  tf.logging.info('generate_len: {}'.format(len(output)))
+  tf.logging.info(line.encode('utf-8'))
+
+def train(config):
+  vocab = data_reader.Vocab(vocab_limits = config['vocab_size'])
+  vocab.load_metadata(config['metadata'])
+  config['vocab_size'] = vocab.vocab_size()
+  tf.logging.info(config)
+
+  train_data = data_reader.DataReader(config['train_data'], \
+                                      vocab = vocab, \
+                                      batch_size = config['batch_size'], \
+                                      seq_length = config['seq_length'])
+  initializer = tf.random_uniform_initializer(-config['init_scale'],
+                                              config['init_scale'])
+  with tf.name_scope('Train'):
+    opt, lr = optimizer.get_optimizer("sgd", config['learning_rate'])
+    with tf.variable_scope("Model", reuse = None, initializer = initializer):
+      train_model = model.Model(is_training = True, \
+                                config = config, \
+                                optimizer = opt,
+                                lr = lr)
+
+  with tf.name_scope('Generate'):
+    generate_config = copy.deepcopy(config)
+    generate_config['batch_size'] = 1
+    generate_config['seq_length'] = 1
+    with tf.variable_scope("Model", reuse = True, initializer = initializer):
+      gen_model = model.Model(is_training = False, config = generate_config)
+
+  tf.logging.info('Start Sess')
+  with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    for i in range(config['n_epoch']):
+      lr_decay = config['lr_decay'] ** max(i + 1 - config['decay_epoch'], 0)
+      train_model.assign_lr(sess, config['learning_rate'] * lr_decay)
+
+      tf.logging.info('Iter {} Start, Learning_rate: {:4f}'.format(i, sess.run(train_model.lr)))
+      costs, iters = run_epoch(sess, train_model, train_data, \
+                               is_training = True,
+                               gen_model = gen_model,
+                               vocab = vocab)
+      tf.logging.info('Iter {}: training_loss:{:4f}'.format(i, costs / iters))
+
+def main(_):
+  config = HParam()
+  train(config)
+
+if __name__ == '__main__':
+  tf.app.run()
---
+++ src/model.py
@@ -0,0 +1,111 @@
+# -*- coding:utf-8 -*-
+import os
+import sys
+import time
+
+import inspect
+import numpy as np
+import tensorflow as tf
+
+class Model():
+  def lstm_cell(self):
+    if 'reuse' in inspect.getargspec(
+        tf.contrib.rnn.BasicLSTMCell.__init__).args:
+      return tf.contrib.rnn.BasicLSTMCell(
+          self.config['hidden_size'], forget_bias = 0.0, state_is_tuple = True,
+          reuse = tf.get_variable_scope().reuse)
+    else:
+      return tf.contrib.rnn.BasicLSTMCell(
+          self.config['hidden_size'], forget_bias = 0.0, state_is_tuple = True)
+
+  def __init__(self, is_training, config, optimizer = None, lr = None):
+    self.config = config
+    batch_size = config['batch_size']
+    seq_length = config['seq_length']
+    hidden_size = config['hidden_size']
+    vocab_size = config['vocab_size']
+
+    self._optimizer = optimizer
+    self._lr = lr
+
+    with tf.name_scope('inputs'):
+      self.input_data = tf.placeholder(tf.int32, [batch_size, seq_length])
+      self.target_data = tf.placeholder(tf.int32, [batch_size, seq_length])
+      self.mask = tf.placeholder(config['data_type'], [batch_size, seq_length])
+
+    with tf.name_scope('model'):
+      basic_cell = self.lstm_cell
+      self.cell = tf.contrib.rnn.MultiRNNCell(
+          [basic_cell()] * config['num_layers'], state_is_tuple = True)
+
+      self._initial_state = self.cell.zero_state(batch_size, config['data_type'])
+
+      with tf.device("/cpu:0"):
+        embedding = tf.get_variable(
+            "embedding", [vocab_size, hidden_size], dtype = config['data_type'])
+        inputs = tf.nn.embedding_lookup(embedding, self.input_data)
+
+      outputs = []
+      state = self._initial_state
+      with tf.variable_scope("RNN"):
+        for time_step in range(seq_length):
+          if time_step > 0: tf.get_variable_scope().reuse_variables()
+          (cell_output, state) = self.cell(inputs[:, time_step, :], state)
+          outputs.append(cell_output)
+
+    with tf.name_scope('loss'):
+      output = tf.reshape(tf.stack(axis = 1, values = outputs), [-1, hidden_size])
+      softmax_w = tf.get_variable(
+          "softmax_w", [hidden_size, vocab_size], dtype = config['data_type'])
+      softmax_b = tf.get_variable(
+          "softmax_b", [vocab_size], dtype = config['data_type'])
+      logits = tf.matmul(output, softmax_w) + softmax_b
+
+      logits = tf.reshape(logits, [batch_size, seq_length, vocab_size])
+      
+      self.probs = tf.nn.softmax(logits)
+
+      loss = tf.contrib.seq2seq.sequence_loss(
+          logits = logits,
+          targets = self.target_data,
+          weights = self.mask,
+          average_across_timesteps=False,
+          average_across_batch=True)
+
+      self._cost = cost = tf.reduce_sum(loss)
+      self._final_state = state
+
+    if not is_training: return
+
+    with tf.name_scope('optimize'):
+      tvars = tf.trainable_variables()
+      grads, _ = tf.clip_by_global_norm(tf.gradients(cost, tvars),
+                                        config['max_grad_norm'])
+      self._train_op = self._optimizer.apply_gradients(zip(grads, tvars),
+                 global_step = tf.contrib.framework.get_or_create_global_step())
+
+    self._new_lr = tf.placeholder(tf.float32, shape=[])
+    self._lr_update = tf.assign(self._lr, self._new_lr)
+
+  @property
+  def initial_state(self):
+    return self._initial_state
+
+  @property
+  def final_state(self):
+    return self._final_state
+
+  @property
+  def train_op(self):
+    return self._train_op
+
+  @property
+  def optimizer(self):
+    return self._optimizer
+
+  @property
+  def lr(self):
+    return self._lr
+
+  def assign_lr(self, sess, new_lr):
+    sess.run(self._lr_update, feed_dict={self._new_lr: new_lr})
