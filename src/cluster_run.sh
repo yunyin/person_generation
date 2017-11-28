59d66bee63ed51aac5127ae254dd701737253946
yunyin <root@a25j01437.em21>
2017-11-28 03:35:17 +0000

    Create Cluster For Training

---
+++ src/cluster_run.sh
@@ -0,0 +1,3 @@
+nohup python generator.py --config=../conf/test.conf --type=train --job_name=ps > log.ps &
+nohup python generator.py --config=../conf/test.conf --type=train --job_name=worker --task_id=0 > log.0 &
+nohup python generator.py --config=../conf/test.conf --type=train --job_name=worker --task_id=1 > log.1 &
--- src/generator.py
+++ src/generator.py
@@ -17,6 +17,10 @@ flags.DEFINE_string("config", None,
                     "Directory of Config File.")
 flags.DEFINE_string("type", None,
                     "Run Type: train/gen")
+flags.DEFINE_integer("task_id", 0,
+                    "Task Id for Each workers")
+flags.DEFINE_string("job_name", 'worker',
+                    "Job Type: ps/worker")
 FLAGS = flags.FLAGS
 tf.logging.set_verbosity(tf.logging.INFO)
 
@@ -84,38 +88,68 @@ def sample(sess, model, vocab, max_gen_len = 50):
   tf.logging.info(line.encode('utf-8'))
 
 def train(config):
+  # load Vocab
   vocab = data_reader.Vocab(vocab_limits = config['vocab_size'])
   vocab.load_metadata(config['metadata'])
   config['vocab_size'] = vocab.vocab_size()
   tf.logging.info(config)
 
-  train_data = data_reader.DataReader(config['train_data'], \
-                                      vocab = vocab, \
-                                      batch_size = config['batch_size'], \
+  # load Data
+  train_data = data_reader.DataReader(config['train_data'][FLAGS.task_id],
+                                      vocab = vocab,
+                                      batch_size = config['batch_size'],
                                       seq_length = config['seq_length'])
+
+  # init for cluster
+  cluster_conf = config["cluster_conf"]
+  cluster = tf.train.ClusterSpec(cluster_conf)
+  server = tf.train.Server(cluster,
+                           job_name = FLAGS.job_name,
+                           task_index = FLAGS.task_id)
+  n_ps = len(cluster_conf['ps'])
+  n_workers = len(cluster_conf['worker'])
+  if FLAGS.job_name == "ps": server.join()
+  is_chief = (FLAGS.task_id == 0)
+
   initializer = tf.random_uniform_initializer(-config['init_scale'],
                                               config['init_scale'])
-  with tf.name_scope('Train'):
-    opt, lr = optimizer.get_optimizer("sgd", config['learning_rate'])
-    with tf.variable_scope("Model", reuse = None, initializer = initializer):
-      train_model = model.Model(is_training = True, \
-                                config = config, \
-                                optimizer = opt,
-                                lr = lr)
 
-  with tf.name_scope('Generate'):
-    generate_config = copy.deepcopy(config)
-    generate_config['batch_size'] = 1
-    generate_config['seq_length'] = 1
-    with tf.variable_scope("Model", reuse = True, initializer = initializer):
-      gen_model = model.Model(is_training = False, config = generate_config)
+  # create models
+  with tf.device(tf.train.replica_device_setter(worker_device="/job:worker/task:%d" % FLAGS.task_id,
+                                                cluster = cluster)):
+    with tf.name_scope('Train'):
+      opt, lr = optimizer.get_optimizer("sgd", config['learning_rate'])
+      sync_opt = tf.train.SyncReplicasOptimizer(opt,
+                                                replicas_to_aggregate = n_workers * config['sync_freq'],
+                                                total_num_replicas = n_workers)
+      with tf.variable_scope("Model", reuse = None, initializer = initializer):
+        train_model = model.Model(is_training = True, \
+                                  config = config, \
+                                  optimizer = sync_opt,
+                                  lr = lr)
+
+    with tf.name_scope('Generate'):
+      generate_config = copy.deepcopy(config)
+      generate_config['batch_size'] = 1
+      generate_config['seq_length'] = 1
+      with tf.variable_scope("Model", reuse = True, initializer = initializer):
+        gen_model = model.Model(is_training = False, config = generate_config)
+
+    chief_queue_runner = train_model.optimizer.get_chief_queue_runner()
+    init_tokens_op = train_model.optimizer.get_init_tokens_op()
+
+  sv = tf.train.Supervisor(is_chief = is_chief,
+                           logdir = config['logdir'])
+  sess_config = tf.ConfigProto(allow_soft_placement = True,
+                               log_device_placement = True,
+                               device_filters = ["/job:ps", "/job:worker/task:%d" % FLAGS.task_id])
 
   tf.logging.info('Start Sess')
+  with sv.prepare_or_wait_for_session(server.target, config=sess_config) as sess:
+    if is_chief:
+      sv.start_queue_runners(sess, [chief_queue_runner])
+      sess.run(init_tokens_op)
 
-  sv = tf.train.Supervisor(logdir = config['logdir'],
-                           save_model_secs = 10)
-  with sv.managed_session() as sess:
-    sample(sess, gen_model, vocab)
     for i in range(config['n_epoch']):
       lr_decay = config['lr_decay'] ** max(i + 1 - config['decay_epoch'], 0)
       train_model.assign_lr(sess, config['learning_rate'] * lr_decay)
